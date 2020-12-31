35cf57136425986fa7ac1da1331ae011267c3062
Tim Vick <t.vick@yahoo.com>
2020-12-31 00:38:36 +0000

    add new scrimmage module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const config = require("./config");
-const scrimmages = require("./services");
+const scrimmages = require("./services/scrimmages");
 
 const client = new Discord.Client();
 
@@ -9,14 +9,16 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (msg) => {
+client.on("message", async (msg) => {
     if (msg.channel.id != config.channel_id) return;
     if (msg.content != "show") return;
 
-    scrimmages
-        .fetch()
-        .then((scrimmages) => msg.reply("```" + scrimmages + "```"))
-        .catch((error) => msg.reply("An error occured"));
+    try {
+        const reply = await scrimmages.fetch();
+        msg.reply("```" + reply + "```");
+    } catch (error) {
+        msg.reply("An error occured");
+    }
 });
 
 client.login(config.discord_token);
