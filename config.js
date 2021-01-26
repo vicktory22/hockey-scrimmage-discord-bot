9af1b69a5817ce05114bf78266fca80ccf207ea8
Tim Vick <t.vick@yahoo.com>
2021-01-26 21:26:25 +0000

    add trailing comma

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     discordToken: process.env.DISCORD_TOKEN,
     channelId: process.env.CHANNEL_ID,
     url: process.env.SITE_URL,
-    googleLogData: process.env.GOOGLE_LOG_DATA
+    googleLogData: process.env.GOOGLE_LOG_DATA,
 };
