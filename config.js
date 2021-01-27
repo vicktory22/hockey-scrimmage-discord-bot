ae4be8d3883d73b0c5cd358ad363804d2e54d980
Tim Vick <t.vick@yahoo.com>
2021-01-27 19:11:55 +0000

    update channel key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     discordToken: process.env.DISCORD_TOKEN,
-    channelId: process.env.CHANNEL_ID,
+    discordChannelId: process.env.CHANNEL_ID,
     url: process.env.SITE_URL,
     googleLogData: process.env.GOOGLE_LOG_DATA,
 };
